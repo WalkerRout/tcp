db3183aff2fd7409a07056f6a66b08f59f2b33e8
walkerrout04@gmail.com <walkerrout04@gmail.com>
2022-11-22 13:25:01 -0800

    Updated server main

--- server/src/main.rs
+++ server/src/main.rs
@@ -28,3 +28,45 @@ impl Server {
     Ok(())
   }
 }
+
+fn handle_connection(mut stream: std::net::TcpStream) -> ResBErr<()> {
+    use std::io::Read;
+
+    let mut buffer = vec![0; 256];
+    stream.read(&mut buffer)?;
+
+    if compare(&buffer[..], &[80, 65, 78, 73, 67]) == std::cmp::Ordering::Equal {
+      panic!("'PANIC' SENT!");
+    }
+    
+    let buffer_string = String::from_utf8_lossy(&buffer);
+
+    println!("-- Data from connection --
+           \nBytes:{:?}
+           \nString Representation: {}\n", 
+           &buffer[..], &buffer_string);
+
+    Ok(())
+  }
+
+fn compare(a: &[u8], b: &[u8]) -> std::cmp::Ordering {
+  for (ai, bi) in a.iter().zip(b.iter()) {
+    match ai.cmp(&bi) {
+      std::cmp::Ordering::Equal => continue,
+      ord => return ord
+    }
+  }
+
+  std::cmp::Ordering::Equal
+}
+  
+fn main() -> ResBErr<()> {
+  let ipp = "127.0.0.1:34254";
+
+  println!("Server Running on {} ...", ipp);
+
+  let mut server = Server::new(ipp)?;
+  server.listen(|stream| handle_connection(stream))?;
+  
+  Ok(())
+}
